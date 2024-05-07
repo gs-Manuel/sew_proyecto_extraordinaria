a01bdedb1e45872c65fb96ea58b0ea853b5a6596
Manuel <gs_manuel@outlook.es>
2024-05-07 19:35:53 +0200

    Empiezo el juego y algo de metereologia

--- js/Metereologia.js
+++ js/Metereologia.js
@@ -2,23 +2,31 @@
 class Metereologia{
     constructor() {
         this.contenedor=document.querySelector("article");
+        this.last_api_call  = null;
+        this.last_api_result = null;
         this.obtenerPrevisionTiempo();
     }
     obtenerPrevisionTiempo() {
-        const apiKey = "af570203c4f84141406f2b72dc283547";
-        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${37.986111}&lon=${-1.130278}&units=metric&appid=${apiKey}`;
-         $.getJSON({
-            url: apiUrl,
-            method: 'GET',
-            success: data =>{
-                this.imprimirPrevisionTiempo(data)
-            },
-            error: error => {
-                console.error(error);
+        if (this.last_api_call === null || this.last_api_call + (2 * 60 * 60 * 1000) < Date.now()) {//una llamada cada 2 horas
+            this.last_api_call = Date.now();
+            const apiKey = "af570203c4f84141406f2b72dc283547";
+            const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${37.986111}&lon=${-1.130278}&units=metric&appid=${apiKey}`;
+            $.getJSON({
+                url: apiUrl,
+                method: 'GET',
+                success: data => {
+                    this.last_api_result = data;
+                    this.imprimirPrevisionTiempo(data);
+                },
+                error: error => {
+                    console.error(error);
+                }
+            });
+        }else {
+                this.imprimirPrevisionTiempo(this.last_api_result);
             }
-        });
     }
-    imprimirPrevisionTiempo(data) {
+    imprimirPrevisionTiempo(data){
         let minTempDay = Number.POSITIVE_INFINITY;
         let maxTempDay = -100000;
         data.list.forEach(element => {
@@ -26,16 +34,26 @@ class Metereologia{
             maxTempDay = element.main.temp_max > maxTempDay ? element.main.temp_max : maxTempDay;
             if(element.dt_txt.includes("18:00:00")){ //Si es la hora de las 15:00
                 const section = document.createElement("section")
-                section.append(`<h4>${element.main.temp} °C</h4>`);
-                section.append(`<p>Temperatura mínima: ${minTempDay} °C</p>`);
-                section.append(`<p>Temperatura máxima: ${maxTempDay} °C</p>`);
-                section.append(`<p>Presión: ${element.main.pressure} °C</p>`);
-                section.append(`<p>Humedad: ${element.main.humidity} %</p>`);
-                section.append(`<p>Cantidad de Lluvia: ${element.rain ? element.rain['3h'] : 0} mm</p>`);
-                const iconUrl = `https://openweathermap.org/img/w/${element.weather[0].icon}.png`;
-                section.append(`<img src="${iconUrl}" alt="Icono del tiempo">`);
+                const title = document.createElement("h4")
+                title.textContent=element.main.temp+" °C";
+                const p1 =document.createElement("p");
+                p1.textContent="Temperatura mínima: "+minTempDay+" °C";
+                const p2 =document.createElement("p");
+                p2.textContent="Temperatura máxima: "+maxTempDay+" °C";
+                const p3 =document.createElement("p");
+                p3.textContent="Presión: "+element.main.pressure+" °C";
+                const p4 =document.createElement("p");
+                p4.textContent="Humedad: "+element.main.humidity+" %";
+                const iconUrl = document.createElement("img");
+                iconUrl.src=`https://openweathermap.org/img/w/${element.weather[0].icon}.png`;
+                iconUrl.alt="Icono del tiempo";
+                section.append(title)
+                section.append(p1)
+                section.append(p2)
+                section.append(p3)
+                section.append(p4)
+                section.append(iconUrl);
                 $("article").append(section);
-
                 minTempDay = Number.POSITIVE_INFINITY;
                 maxTempDay = -100000;
             }

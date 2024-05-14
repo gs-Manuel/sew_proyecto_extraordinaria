f90ab084a83973a075caf6dbcb677163bb36431b
Manuel <gs_manuel@outlook.es>
2024-05-14 16:06:53 +0200

    Mapa y Noticias

--- js/Mapa.js
+++ js/Mapa.js
@@ -1,80 +1,25 @@
 "use-strict"
 class Mapa{
     constructor (){
-        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.verErrores.bind(this));
+        this.apiKeyGoogleMaps = "&key=AIzaSyDNPnDPlm8m8ot24z5jQ0gWSKYoqCb1ego";
+        this.longitud         = -1.130278;
+        this.latitud          = 37.986111;
+        this.getMapaEstaticoGoogle();
     }
-    getPosicion(posicion){
-        this.mensaje = "Se ha realizado correctamente la petición de geolocalización";
-        this.longitud         = posicion.coords.longitude;
-        this.latitud          = posicion.coords.latitude;
-        this.precision        = posicion.coords.accuracy;
-        this.altitud          = posicion.coords.altitude;
-        this.precisionAltitud = posicion.coords.altitudeAccuracy;
-        this.rumbo            = posicion.coords.heading;
-        this.velocidad        = posicion.coords.speed;
-    }
-    verErrores(error){
-        switch(error.code) {
-            case error.PERMISSION_DENIED:
-                this.mensaje = "El usuario no permite la petición de geolocalización"
-                break;
-            case error.POSITION_UNAVAILABLE:
-                this.mensaje = "Información de geolocalización no disponible"
-                break;
-            case error.TIMEOUT:
-                this.mensaje = "La petición de geolocalización ha caducado"
-                break;
-        }
-    }
-    getLongitud(){
-        return this.longitud;
-    }
-    getLatitud(){
-        return this.latitud;
-    }
-    getAltitud(){
-        return this.altitud;
-    }
-    verTodo(dondeVerlo){
-        var ubicacion=document.getElementById(dondeVerlo);
-        var datos='<p>'+ this.mensaje + '</p>';
-        datos+='<p>Longitud: '+this.longitud +' grados</p>';
-        datos+='<p>Latitud: '+this.latitud +' grados</p>';
-        datos+='<p>Precisión de la longitud y latitud: '+ this.precision +' metros</p>';
-        datos+='<p>Altitud: '+ this.altitude +' metros</p>';
-        datos+='<p>Precisión de la altitud: '+ this.precisionAltitud +' metros</p>';
-        datos+='<p>Rumbo: '+ this.rumbo +' grados</p>';
-        datos+='<p>Velocidad: '+ this.velocidad +' metros/segundo</p>';
-        ubicacion.innerHTML = datos;
-    }
-    getMapaEstaticoGoogle(dondeVerlo){
-        var ubicacion=document.getElementById(dondeVerlo);
-
-        const apiKeyGoogleMaps = "AIzaSyDNPnDPlm8m8ot24z5jQ0gWSKYoqCb1ego";
-
-        //URL: obligatoriamente https
+    getMapaEstaticoGoogle(){
+        var ubicacion=document.querySelector("article:nth-of-type(3)");
         var url = "https://maps.googleapis.com/maps/api/staticmap?";
-        //Parámetros
-        // centro del mapa (obligatorio si no hay marcadores)
         var centro = "center=" + this.latitud + "," + this.longitud;
-        //zoom (obligatorio si no hay marcadores)
-        //zoom: 1 (el mundo), 5 (continentes), 10 (ciudad), 15 (calles), 20 (edificios)
         var zoom ="&zoom=15";
-        //Tamaño del mapa en pixeles (obligatorio)
         var tamaño= "&size=800x600";
-        //Escala (opcional)
-        //Formato (opcional): PNG,JPEG,GIF
-        //Tipo de mapa (opcional)
-        //Idioma (opcional)
-        //region (opcional)
-        //marcadores (opcional)
+        let idioma="&language=es"
         var marcador = "&markers=color:red%7Clabel:S%7C" + this.latitud + "," + this.longitud;
-        //rutas. path (opcional)
-        //visible (optional)
-        //style (opcional)
         var sensor = "&sensor=false";
 
-        this.imagenMapa = url + centro + zoom + tamaño + marcador + sensor + apiKeyGoogleMaps;
-        ubicacion.innerHTML = "<img src='"+this.imagenMapa+"' alt='mapa estático google' />";
+        this.imagenMapa = url + centro + zoom + tamaño +idioma+ marcador + sensor + this.apiKeyGoogleMaps;
+        let imagen = document.createElement("img");
+        imagen.src=this.imagenMapa;
+        imagen.alt='Mapa estático google';
+        ubicacion.appendChild(imagen);
     }
 }
--- js/Noticias.js
+++ js/Noticias.js
@@ -1,14 +1,50 @@
 "use-strict"
 class Noticias{
     constructor(){
-        this.apiKey="ef0eac5b55c74c8a8ae28419042f9bcc";
-        this.apiUrl = `https://newsapi.org/v2/everything?q=murcia&language=es&to=2024-05-10&sortBy=relevancy&apiKey=${apiKey}`;
-        this.mostrarNoticias();
+        this.date= new Date(Date.now())
+        this.apiKey= "ef0eac5b55c74c8a8ae28419042f9bcc";
+        this.apiUrl = `https://newsapi.org/v2/everything?q=murcia&language=es&to=${this.date.toLocaleDateString()}&sortBy=relevancy&apiKey=${this.apiKey}`;
+        this.last_api_call  = null;
+        this.last_api_result = null;
+        this.obtenerNoticias();
     }
-    async mostrarNoticias(){
-        const response = await fetch(this.apiUrl);
-        const responseJSON = await response.json();
+    async obtenerNoticias(){
+        if (this.last_api_call === null || this.last_api_call + (60 * 60 * 1000) < Date.now()) {//una llamada cada hora
+            const response = await fetch(this.apiUrl);
+            const responseJSON = await response.json();
+            this.last_api_call =new Date(Date.now());
+            this.last_api_result=responseJSON;
+            this.mostrarNoticias(responseJSON)
 
+        }else {
+            this.mostrarNoticias(this.last_api_result);
+        }
+    }
+    async mostrarNoticias(data){
+        console.log(data)
+        const contenedorNoticias = document.querySelector("article:last-of-type")
+        let lastApiCallArticle = document.createElement("article")
+        let lastApiCall = document.createElement("p");
+        lastApiCall.textContent+="La última vez que se cargaron noticias fue: "+this.last_api_call.toLocaleString('es');
+        lastApiCallArticle.appendChild(lastApiCall);
+        contenedorNoticias.appendChild(lastApiCallArticle);
+        for (let i = 0; i < 4; i++) {
+            let section = document.createElement("section")
+            let title = document.createElement("h3")
+            let image = document.createElement("img")
+            let description = document.createElement("p")
+            let link = document.createElement("a")
+            title.textContent=data.articles[i].title;
+            image.src=data.articles[i].urlToImage;
+            description.textContent=data.articles[i].description;
+            link.href=data.articles[i].url;
+            link.textContent=data.articles[i].source.name;
+            section.appendChild(title)
+            section.appendChild(image)
+            section.appendChild(description)
+            section.appendChild(link)
+            contenedorNoticias.appendChild(section)
+        }
     }
 }
 
